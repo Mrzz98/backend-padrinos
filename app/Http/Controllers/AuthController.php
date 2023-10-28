a5478294b12353b3cfcdd9ed126d190c23eb75ca
marcelo.ramirez1 <marcelo.ramirez@sekparaguay.com>
2023-10-28 00:46:15 -0300

    test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,17 +15,18 @@ class AuthController extends Controller
         $credentials = $request->only('nombre_usuario', 'contrasena');
     
         if (Auth::attempt($credentials)) {
-            // Obtén el usuario autenticado como instancia de tu modelo Usuario
-            $user = Usuario::find(Auth::user()->id);
+            return response()->json(['error' => 'aaaa incorrectas'], 401);
+            // // Obtén el usuario autenticado como instancia de tu modelo Usuario
+            // $user = Usuario::find(Auth::user()->id);
     
-            // Genera un token de acceso personalizado
-            $token = Str::random(60);
+            // // Genera un token de acceso personalizado
+            // $token = Str::random(60);
     
-            // Asocia el token al usuario y guárdalo en la base de datos
-            $user->api_token = $token;
-            $user->save();
+            // // Asocia el token al usuario y guárdalo en la base de datos
+            // $user->api_token = $token;
+            // $user->save();
     
-            return response()->json(['user' => $user, 'token' => $token], 200);
+            // return response()->json(['user' => $user, 'token' => $token], 200);
         }
     
         return response()->json(['error' => 'Credenciales incorrectas'], 401);
