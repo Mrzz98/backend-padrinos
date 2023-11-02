7ad89bdb15f124797353ca13830764cadeb18e38
marcelo.ramirez1 <marcelo.ramirez@sekparaguay.com>
2023-11-02 00:42:39 -0300

    prueba4

--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,14 @@ use App\Http\Controllers\AuthController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 Route::middleware(['api'])->group(function() {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
+    
     Route::get('/getaccount', [AuthController::class, 'getaccount']);
 });
 Route::get('/usuarios', [UserController::class, 'index']);
+
 // Route::post('/login', [AuthController::class, 'login']);
 
 // Route::post('/logout', 'AuthController@logout');
