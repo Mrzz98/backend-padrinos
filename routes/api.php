446e258342ccdd007349a57c2515c2ed5a328c7c
marcelo.ramirez1 <marcelo.ramirez@sekparaguay.com>
2023-11-02 23:13:43 -0300

    prueba5

--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,8 @@ use App\Http\Controllers\AuthController;
 // });
 
 Route::middleware(['api'])->group(function() {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::get('/getaccount', [AuthController::class, 'getaccount']);
 });
 
