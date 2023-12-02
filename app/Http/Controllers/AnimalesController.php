838fba70c71940a3bd9a1b543d06b109db6652fa
marcelo.ramirez1 <marcelo.ramirez@sekparaguay.com>
2023-12-02 00:58:10 -0300

    prueba

--- app/Http/Controllers/AnimalesController.php
+++ app/Http/Controllers/AnimalesController.php
@@ -91,7 +91,7 @@ class AnimalesController extends Controller
             'tamano' => 'string',
             'edad' => 'integer',
             'descripcion' => 'string',
-            'image' => 'required|image|mimes:png,jpg,jpeg|max:2048'
+            'image' => 'string'
         ]);
 
         $imageName = "a";
